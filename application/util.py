c4cf9b8daa18f17fd5e6f8889111dd9385e6ca69
richxrdkim <richarddkim55@gmail.com>
2025-09-09 20:38:24 -0700

    redeploy

--- application/util.py
+++ application/util.py
@@ -14,7 +14,7 @@ def make_token(user_id: int, role: str = "user") -> str:
 
 
 def token_required(fn):
-    @wraps(f)
+    @wraps(fn)
     def wrapper(*args, **kwargs):
         auth = request.headers.get("Authorization", "")
         if not auth.startswith("Bearer "):
